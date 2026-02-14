47936cd4fa9ff87e9216750a471040a70a42caee
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-02-15 02:46:41 +0530

    use turbo_stream instead of custom action cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,15 @@
 class MessagesController < ApplicationController
+  def create
+    @message = current_user.messages.build(message_params)
+    @message.save
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def message_params
+    params.expect(message: [ :body ])
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :user
+  after_create_commit -> { broadcast_append_to "messages", target: "message-display" }
 end
